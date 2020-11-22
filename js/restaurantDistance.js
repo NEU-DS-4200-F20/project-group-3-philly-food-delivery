a232f8470726817d38542449a8ffa4887fb4318a
Benjamin Jacobson <jacobson.b@husky.neu.edu>
2020-11-22 17:21:49 -0500

    changed labels for both bar charts

--- js/restaurantDistance.js
+++ js/restaurantDistance.js
@@ -3,34 +3,34 @@ function barChart2() {
 	function chart(selector, data, dispatch) {
 
 		// creating map of the data by grouping by restaurants
-		let restaurantsOrderDistance = d3.rollup(data, v=> d3.mean(v, d => d.distance_miles_a2b), d => d._seller_id)
-		
+		let restaurantsOrderDistance = d3.rollup(data, v => d3.mean(v, d => d.distance_miles_a2b), d => d._seller_id)
+
 
 		// getting max delivery distance for y-axis scale
 		let maxCount = d3.max(restaurantsOrderDistance.values())
 
 		// setting dimensions
 		let width = 250,
-		height = 250,
-		margin = {
-			top: 30,
-			bottom: 30,
-			left: 50,
-			right: 30
-		};
+			height = 250,
+			margin = {
+				top: 30,
+				bottom: 30,
+				left: 50,
+				right: 30
+			};
 
 		//creating svg
 		let svg = d3.select(selector)
 			.append('svg')
-				.attr('width', width)
-				.attr('height', height)
-				.style('background', 'white')
-				.attr('x', 500)
-				.attr('y', 20);
+			.attr('width', width)
+			.attr('height', height)
+			.style('background', 'white')
+			.attr('x', 500)
+			.attr('y', 20);
 
 		let chartGroup = svg
 			.append('g')
-				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 		// creating y axis scale
 		let yScale = d3.scaleLinear()
@@ -42,24 +42,26 @@ function barChart2() {
 		chartGroup.append('g')
 			.attr('class', 'y axis')
 			.attr('transform', 'translate(0, 0)')
-			.call(yAxis)
-			//Add label
-			.append('text')
-			.attr('x', -80)
-			.attr('y', -30)
-			.style('stroke', 'black')
+			.call(yAxis);
+
+		//Add label	
+		svg.append('text')
+			.attr('x', -130)
+			.attr('y', 20)
+			//.style('stroke', 'black')
 			.text('Miles')
-			.style('font-size', '8px')
+			.style('font-size', '10px')
 			.attr('transform', 'rotate(-90)');
+		//.call(yAxis);
 
 		// getting restaurant names from the map
 		let keys = Array.from(restaurantsOrderDistance.keys());
-	
+
 
 		// creating x axis scale
 		let xScale = d3.scaleBand()
-			.range([0, width - margin.right-50])
-			.domain(keys.sort((a,b) =>a-b))
+			.range([0, width - margin.right - 50])
+			.domain(keys.sort((a, b) => a - b))
 			.padding(.1)
 
 		// creating x axis on the page
@@ -67,59 +69,61 @@ function barChart2() {
 
 		// creating bars for the bar chart
 		let bar = chartGroup.append('g')
-		  .selectAll('rect')
-		  .data(restaurantsOrderDistance)
-		  .enter()
-		  .append('rect')
-		  .attr('x', function(d) {
-		  	return xScale(d[0]);
-		  })
-		  .attr('y', function(d) {
-		    return yScale(restaurantsOrderDistance.get(d[0]));
-		  })
-		  .attr('width', xScale.bandwidth())
-		  .attr('fill', 'steelblue')
-		  .attr('height', function(d) {
-		    return height - margin.bottom - 30 - yScale(restaurantsOrderDistance.get(d[0]));
-		  })
-		  .attr('id', function(d) {
-		  	return "rest" + d[0]
-		  })
-		  .on('mouseover', function(d) { //mouseover event
-			  
-			d3.select(this)
-				.style('fill', 'red');
-				// console.log(this)
+			.selectAll('rect')
+			.data(restaurantsOrderDistance)
+			.enter()
+			.append('rect')
+			.attr('x', function (d) {
+				return xScale(d[0]);
+			})
+			.attr('y', function (d) {
+				return yScale(restaurantsOrderDistance.get(d[0]));
+			})
+			.attr('width', xScale.bandwidth())
+			.attr('fill', 'steelblue')
+			.attr('height', function (d) {
+				return height - margin.bottom - 30 - yScale(restaurantsOrderDistance.get(d[0]));
+			})
+			.attr('id', function (d) {
+				return "rest" + d[0]
+			})
+			.on('mouseover', function (d, i) { //mouseover event
+
+				d3.select(this)
+					.style('fill', 'red');
+				console.log(i)
 				dispatch.call('mouseOver', this, d3.select(this).attr('id'));
 			})
-		  .on('mouseout', function(d) { //mouseout event
-			d3.select(this)
-				.style('fill', 'steelblue')
+			.on('mouseout', function (d) { //mouseout event
+				d3.select(this)
+					.style('fill', 'steelblue')
 				dispatch.call('mouseOut', this, d3.select(this).attr('id'));
 			});
 
-		  // dispatch mousover listener event
-		  dispatch.on("mouseOver" + ".b", function(id) {
-		  	d3.selectAll('#' + id)
-		  		.style('fill', 'red')
-		  })
-		  // dispatch mouseout listener event
-		  dispatch.on("mouseOut" + ".b", function(id) {
-		  	d3.selectAll('#' + id)
-		  		.style('fill', 'steelblue')
-		  })
+		// dispatch mousover listener event
+		dispatch.on("mouseOver" + ".b", function (id) {
+			d3.selectAll('#' + id)
+				.style('fill', 'red')
+		})
+		// dispatch mouseout listener event
+		dispatch.on("mouseOut" + ".b", function (id) {
+			d3.selectAll('#' + id)
+				.style('fill', 'steelblue')
+		})
 
 		chartGroup.append('g')
-	  		.attr('class', 'x axis')
-	  		.attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
-	  		.call(xAxis)
-	  		//Add label
-			.append('text')
-			.attr('x', (width - 50) / 2 - 10)
-			.attr('y', 25)
-			.style('stroke', 'black')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
+			.call(xAxis);
+
+
+		//Add label
+		svg.append('text')
+			.attr('x', 110)
+			.attr('y', 245)
+			//.style('stroke', 'black')
 			.text('Restaurants')
-			.style('font-size', '8px');
+			.style('font-size', '10px');
 
 		// adding title
 		svg.append('text')
--- js/restaurantOrderCount.js
+++ js/restaurantOrderCount.js
@@ -3,8 +3,8 @@ function barChart1() {
 	function chart(selector, data, dispatch) {
 
 		//creating map of the data by grouping by restaurants
-		let restaurantsOrderCount = d3.rollup(data, v => v.length, d=>d._seller_id)
-		
+		let restaurantsOrderCount = d3.rollup(data, v => v.length, d => d._seller_id)
+
 
 		// getting max order count for y-axis scale
 		let maxCount = d3.max(restaurantsOrderCount.values())
@@ -12,26 +12,26 @@ function barChart1() {
 
 		// setting dimensions
 		let width = 250,
-		height = 250,
-		margin = {
-			top: 30,
-			bottom: 30,
-			left: 50,
-			right: 30
-		};
+			height = 250,
+			margin = {
+				top: 30,
+				bottom: 30,
+				left: 50,
+				right: 30
+			};
 
 		//creating svg
 		let svg = d3.select(selector)
 			.append('svg')
-				.attr('width', width)
-				.attr('height', height)
-				.style('background', 'white')
-				.attr('x', 80)
-				.attr('y', 20);
+			.attr('width', width)
+			.attr('height', height)
+			.style('background', 'white')
+			.attr('x', 80)
+			.attr('y', 20);
 
 		let chartGroup = svg
 			.append('g')
-				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 		// creating y axis scale
 		let yScale = d3.scaleLinear()
@@ -43,14 +43,15 @@ function barChart1() {
 		chartGroup.append('g')
 			.attr('class', 'y axis')
 			.attr('transform', 'translate(0, 0)')
-			.call(yAxis)
-			//Add label
-			.append('text')
-			.attr('x', -80)
-			.attr('y', -40)
-			.style('stroke', 'black')
+			.call(yAxis);
+
+		//Add label
+		svg.append('text')
+			.attr('x', -130)
+			.attr('y', 10)
+			//.style('stroke', 'black')
 			.text('Orders')
-			.style('font-size', '8px')
+			.style('font-size', '10px')
 			.attr('transform', 'rotate(-90)');
 
 		// getting restaurant names from the map
@@ -59,8 +60,8 @@ function barChart1() {
 
 		// creating x axis scale
 		let xScale = d3.scaleBand()
-			.range([0, width - margin.right-50])
-			.domain(keys.sort((a,b) =>a-b))
+			.range([0, width - margin.right - 50])
+			.domain(keys.sort((a, b) => a - b))
 			.padding(.1)
 
 		// creating s axis on the page
@@ -68,57 +69,57 @@ function barChart1() {
 
 		// creating bars for the bar chart
 		let bar = chartGroup.append('g')
-		  .selectAll('rect')
-		  .data(restaurantsOrderCount)
-		  .enter()
-		  .append('rect')
-		  .attr('x', function(d) {
-		  	return xScale(d[0]);
-		  })
-		  .attr('y', function(d) {
-		    return yScale(restaurantsOrderCount.get(d[0]));
-		  })
-		  .attr('width', xScale.bandwidth())
-		  .attr('fill', 'steelblue')
-		  .attr('height', function(d) {
-		    return height - margin.bottom - 30 - yScale(restaurantsOrderCount.get(d[0]));
-		  })
-		  .attr('id', function(d) {
-		  	return "rest" + d[0]
-		  })
-		  .on('mouseover', function(d) { //mouseover event
-		  	d3.select(this)
-		  		.style('fill', "red")
-		  	dispatch.call('mouseOver', this, d3.select(this).attr('id'));		  	
-		  })
-		  .on('mouseout', function(d) { //mouseout event
-			  d3.select(this)
-				  .style('fill', 'steelblue')
-			dispatch.call('mouseOut', this, d3.select(this).attr('id'));
-		  });
+			.selectAll('rect')
+			.data(restaurantsOrderCount)
+			.enter()
+			.append('rect')
+			.attr('x', function (d) {
+				return xScale(d[0]);
+			})
+			.attr('y', function (d) {
+				return yScale(restaurantsOrderCount.get(d[0]));
+			})
+			.attr('width', xScale.bandwidth())
+			.attr('fill', 'steelblue')
+			.attr('height', function (d) {
+				return height - margin.bottom - 30 - yScale(restaurantsOrderCount.get(d[0]));
+			})
+			.attr('id', function (d) {
+				return "rest" + d[0]
+			})
+			.on('mouseover', function (d) { //mouseover event
+				d3.select(this)
+					.style('fill', "red")
+				dispatch.call('mouseOver', this, d3.select(this).attr('id'));
+			})
+			.on('mouseout', function (d) { //mouseout event
+				d3.select(this)
+					.style('fill', 'steelblue')
+				dispatch.call('mouseOut', this, d3.select(this).attr('id'));
+			});
 
-		  //dispatch mouse listener event
-		  dispatch.on("mouseOver" + ".a", function(id) {
-		  	d3.selectAll('#' + id)
-		  		.style('fill', 'red')
-		  })
-		  // dispatch mouseout listener event
-		  dispatch.on("mouseOut" + ".a", function(id) {
-		  	d3.selectAll('#' + id)
-		  		.style('fill', 'steelblue')
-		  })
+		//dispatch mouse listener event
+		dispatch.on("mouseOver" + ".a", function (id) {
+			d3.selectAll('#' + id)
+				.style('fill', 'red')
+		})
+		// dispatch mouseout listener event
+		dispatch.on("mouseOut" + ".a", function (id) {
+			d3.selectAll('#' + id)
+				.style('fill', 'steelblue')
+		})
 
 		chartGroup.append('g')
-	  		.attr('class', 'x axis')
-	  		.attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
-	  		.call(xAxis)
-	  		//Add label
-			.append('text')
-			.attr('x', (width - 50) / 2 - 10)
-			.attr('y', 25)
-			.style('stroke', 'black')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
+			.call(xAxis);
+		//Add label
+		svg.append('text')
+			.attr('x', 110)
+			.attr('y', 245)
+			//.style('stroke', 'black')
 			.text('Restaurants')
-			.style('font-size', '8px');
+			.style('font-size', '10px');
 
 		// adding title
 		svg.append('text')
@@ -139,10 +140,10 @@ function barChart1() {
 		return chart;
 	}
 
-	chart.selectionDispatcher = function(_) {
+	chart.selectionDispatcher = function (_) {
 		if (!arguments.length) return dispatcher;
-    	dispatcher = _;
-    	return chart;
+		dispatcher = _;
+		return chart;
 	}
 
 	return chart;
